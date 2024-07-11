68be3d9ba658db2f5bea0ee225e4dc3a0f7913db
Niall Caplis <niallcaplis@gmail.com>
2024-07-11 13:16:00 +0000

    Set private env variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zf+wjjo81v!-ha69av&ov4yf*ypx6hz#k3z+d!5ywd!j6*(6wi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
